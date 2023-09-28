356db7e421c2199abdaa041b6edbbb74649d5f5c
zayedh80 <zayedh80@gmail.com>
2023-09-28 15:33:26 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tp2 ex1"
+include(":app")
